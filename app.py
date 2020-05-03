b2df9c35bc83e2eb229533c765ae797f00c4bdde
Abhay Raj Singh <abhay.singh@app-scoop.com>
2020-05-03 20:30:31 +0530

    Making cards responsive

--- app.py
+++ app.py
@@ -2,38 +2,49 @@ import covid
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# common variables
+GITHUB_REFERENCE = "<h5><a href='https://github.com/ChiragSaini/' target='_blank'>Github</a></h5>"
+LINKED_IN_REFERENCE = "<h5><a href='https://www.linkedin.com/in/chiragsaini97/' target='_blank'>LinkedIn</a></h5>"
+TWITTER_REFERENCE = "<h5><a href='https://twitter.com/ChiragSaini97' target='_blank'>Twitter</a></h5>"
+SUBHEAD_TITLE = "Covid19 Dashboard"
+SUBHEAD_CREDITS = "Made by Chirag Saini"
 c = covid.Covid()
 
 
 # converts DataFrame to presentable card view in rows
 def get_ui_for_data(data_input):
-    column_properties = "margin:2%; padding:1%; float:right; width:15%; text-align: right"
-    base_card_styling = "margin:2%; padding:1%; " \
-                        "text-align:center;" \
-                        "width:15%; " \
-                        "box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);"
-    return f"<div class='row ml-4'>" \
-           f"<h4 style='color:black; {column_properties}'>{data_input[0]}</h4>" \
-           f"<h4 style='color:blue; {base_card_styling}'><ul>Confirmed</ul>{data_input[1]}</h4> " \
-           f"<h4 style='color:orange; {base_card_styling}'><ul>Deaths</ul>{data_input[2]}</h4> " \
-           f"<h4 style='color:green; {base_card_styling}'><ul>Recovered</ul>{data_input[3]}</h4> " \
-           f"<h4 style='color:red; {base_card_styling}'><ul>Active</ul>{data_input[4]}</h4> " \
+    smaller = "font-size:inherit; font-size:65%; font-weight:light;"
+    larger = "font-size:inherit; font-size:110%"
+    confirmed_card_data = f"<h6 style='{smaller}'>Confirmed<h5 style='{larger}'>{data_input[1]}</h5></h6>"
+    death_card_data = f"<h6 style='{smaller}'>Deaths<h5 style='{larger}'>{data_input[1]}</h5></h6>"
+    recovered_card_data = f"<h6 style='{smaller}'>Recovered<h5 style='{larger}'>{data_input[1]}</h5></h6>"
+    active_card_data = f"<h6 style='{smaller}'>Active<h5 style='{larger}'>{data_input[1]}</h5></h6>"
+    country_card_style = "margin:2%; width:15%; text-align:right; font-size:inherit;"
+    base_card_style = "margin:2%; width:15%; text-align:center; font-size:inherit; " \
+                      "box-shadow:0 4px 8px 0 rgba(0,0,0,0.2);"
+    return f"<div class='row' style='font-size:2vw;'>" \
+           f"<h4 style='{country_card_style}'>{data_input[0]}</h4>" \
+           f"<h4 style='color:blue; {base_card_style}'>{confirmed_card_data}</h4> " \
+           f"<h4 style='color:orange; {base_card_style}'>{death_card_data}</h4> " \
+           f"<h4 style='color:green; {base_card_style}'>{recovered_card_data}</h4> " \
+           f"<h4 style='color:red; {base_card_style}'>{active_card_data}</h4> " \
            f"</div>"
 
 
 @st.cache
 def load_data_and_return_dataframe():
     all_cases = c.get_data()
-    df = pd.DataFrame({
-    'Country': [i['country'] for i in all_cases],
-    'Confirmed': [i['confirmed'] for i in all_cases],
-    'Deaths': [i['deaths'] for i in all_cases],
-    'Recovered': [i['recovered'] for i in all_cases],
-    'Active': [i['active'] for i in all_cases]
-    # 'latitude': [i['attributes']['Lat'] for i in all_cases],
-    # 'longitude': [i['attributes']['Long_'] for i in all_cases]
+    return pd.DataFrame({
+        'Country': [i['country'] for i in all_cases],
+        'Confirmed': [i['confirmed'] for i in all_cases],
+        'Deaths': [i['deaths'] for i in all_cases],
+        'Recovered': [i['recovered'] for i in all_cases],
+        'Active': [i['active'] for i in all_cases]
+        # 'latitude': [i['attributes']['Lat'] for i in all_cases],
+        # 'longitude': [i['attributes']['Long_'] for i in all_cases]
     })
-    return df
+
 
 df = load_data_and_return_dataframe()
 st.title("COVID19 DashBoard")
@@ -49,7 +60,7 @@ if sel_country:
         st.markdown(get_ui_for_data(data), unsafe_allow_html=True)
 
 st.markdown("<h2>Top 10 Countries affected</h2>", unsafe_allow_html=True)
-ax  =df[:10].plot(kind='bar', legend=True, fontsize=8)
+ax = df[:10].plot(kind='bar', legend=True, fontsize=8)
 plt.ticklabel_format(axis="y", style="plain", scilimits=None)
 ax.set_xticklabels(df[:10]["Country"])
 st.pyplot()
@@ -59,8 +70,9 @@ for data in df[:10].values:
 st.markdown("<h3> All Countries Data</h3>", unsafe_allow_html=True)
 st.dataframe(df)
 
-st.sidebar.subheader("Covid19 Dashboard")
-st.sidebar.subheader("Made by Chirag Saini")
-st.sidebar.markdown("<h5><a href='https://github.com/ChiragSaini/' target='_blank'>Github</a></h5>", unsafe_allow_html=True)
-st.sidebar.markdown("<h5><a href='https://www.linkedin.com/in/chiragsaini97/' target='_blank'>LinkedIn</a></h5>", unsafe_allow_html=True)
-st.sidebar.markdown("<h5><a href='https://twitter.com/ChiragSaini97' target='_blank'>Twitter</a></h5>", unsafe_allow_html=True)
+# navigation (sidebar) properties
+st.sidebar.subheader(SUBHEAD_TITLE)
+st.sidebar.subheader(SUBHEAD_CREDITS)
+st.sidebar.markdown(GITHUB_REFERENCE, unsafe_allow_html=True)
+st.sidebar.markdown(LINKED_IN_REFERENCE, unsafe_allow_html=True)
+st.sidebar.markdown(TWITTER_REFERENCE, unsafe_allow_html=True)
