9cb5d0a8e0d3dfa62fdb783c324d3aa4d35592c1
Chirag Saini <chiragsaini7777@gmail.com>
2020-06-09 19:28:29 +0530

    Covid19 India Dashboard

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-import covid
+from covid_india import states
 import streamlit as st
+import plotly.graph_objects as go
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
 
 # common variables
 GITHUB_REFERENCE = "<h5><a href='https://github.com/ChiragSaini/' target='_blank'>Github</a></h5>"
@@ -9,7 +12,7 @@ LINKED_IN_REFERENCE = "<h5><a href='https://www.linkedin.com/in/chiragsaini97/' 
 TWITTER_REFERENCE = "<h5><a href='https://twitter.com/ChiragSaini97' target='_blank'>Twitter</a></h5>"
 SUBHEAD_TITLE = "Covid19 Dashboard"
 SUBHEAD_CREDITS = "Made by Chirag Saini"
-c = covid.Covid()
+# c = covid.Covid()
 
 
 # converts DataFrame to presentable card view in rows
@@ -22,59 +25,50 @@ def get_ui_for_data(data_input):
     return f"<div class='row'>" \
            f"<h5 style='color:black; {column_properties}'>{data_input[0]}</h5>" \
            f"<h5 style='color:blue; {base_card_styling}'><h6>Total</h6>{data_input[1]}</h5> " \
-           f"<h5 style='color:red; {base_card_styling}'><h6>Deaths</h6>{data_input[2]}</h5> " \
+           f"<h5 style='color:red; {base_card_styling}'><h6>Active</h6>{data_input[2]}</h5> " \
            f"<h5 style='color:green; {base_card_styling}'><h6>Saved</h6>{data_input[3]}</h5> " \
-           f"<h5 style='color:orange; {base_card_styling}'><h6>Active</h6>{data_input[4]}</h5> " \
+           f"<h5 style='color:orange; {base_card_styling}'><h6>Deaths</h6>{data_input[4]}</h5> " \
            f"</div>"
-
 
 @st.cache
 def load_data_and_return_dataframe():
-    all_cases = c.get_data()
-    return pd.DataFrame({
-        'Country': [i['country'] for i in all_cases],
-        'Confirmed': [i['confirmed'] for i in all_cases],
-        'Deaths': [i['deaths'] for i in all_cases],
-        'Recovered': [i['recovered'] for i in all_cases],
-        'Active': [i['active'] for i in all_cases]
-        # 'latitude': [i['attributes']['Lat'] for i in all_cases],
-        # 'longitude': [i['attributes']['Long_'] for i in all_cases]
-    })
-
+    all_cases = states.getdata()
+    data = []
+    for state in all_cases:
+        temp = []
+        temp.append(state)
+        for key in all_cases[state]:
+            temp.append(all_cases[state][key])
+        data.append(temp)
+    return pd.DataFrame(data, columns=["State", "Total", "Active", "Cured", "Death"])
 
 df = load_data_and_return_dataframe()
-st.title("COVID19 DashBoard")
+st.title("COVID19 India DashBoard")
 st.header("COVID19 Dashboard made using Python and Streamlit")
-st.subheader("Check any Country Status")
-sel_country = st.multiselect(label="Select Country", options=df["Country"])
-if sel_country:
-    countries = []
-    for c in sel_country:
-        countries.append(df[df["Country"] == c].values)
-    for arr in countries:
+st.subheader("Check any State Status")
+sel_state = st.multiselect(label="Select State", options=df["State"])
+if sel_state:
+    states = []
+    for c in sel_state:
+        states.append(df[df["State"] == c].values)
+    for arr in states:
         data = arr[0]
-        data[1] = str(round(data[1] / 1000, 1)) + " K"
-        data[2] = str(round(data[2] / 1000, 1)) + " K"
-        data[3] = str(round(data[3] / 1000, 1)) + " K"
-        data[4] = str(round(data[4] / 1000, 1)) + " K"
-        # data[2] = str(data[2] / 1000) + " K"
-        # data[3] = str(data[3] / 1000) + " K"
-        # data[4] = str(data[4] / 1000) + " K"
         st.markdown(get_ui_for_data(data), unsafe_allow_html=True)
 
-st.markdown("<h2>Top 10 Countries affected</h2>", unsafe_allow_html=True)
-ax = df[:10].plot(kind='bar', legend=True, fontsize=8, color=['blue', 'red', 'green', 'orange'])
-plt.ticklabel_format(axis="y", style="plain", scilimits=None)
-ax.set_xticklabels(df[:10]["Country"])
-st.pyplot()
-for data in df[:10].values:
-    data[1] = str(round(data[1] / 1000, 1)) + " K"
-    data[2] = str(round(data[2] / 1000, 1)) + " K"
-    data[3] = str(round(data[3] / 1000, 1)) + " K"
-    data[4] = str(round(data[4] / 1000, 1)) + " K" 
+st.markdown("<h2>Top 10 States affected</h2>", unsafe_allow_html=True)
+top_10 = df.nlargest(10, ["Total"])
+fig = go.Figure(data = [
+    go.Bar(name="Total", x=top_10["State"], y=top_10["Total"]),
+    go.Bar(name="Active", x=top_10["State"], y=top_10["Active"]),
+    go.Bar(name="Cured", x=top_10["State"], y=top_10["Cured"]),
+    go.Bar(name="Deaths", x=top_10["State"], y=top_10["Death"])
+])
+st.plotly_chart(fig, use_container_width=True)
+
+for data in df.nlargest(10, ["Total"]).values:
     st.markdown(get_ui_for_data(data), unsafe_allow_html=True)
 
-st.markdown("<h3> All Countries Data</h3>", unsafe_allow_html=True)
+st.markdown("<h3> All States Data</h3>", unsafe_allow_html=True)
 st.dataframe(df)
 
 # navigation (sidebar) properties
